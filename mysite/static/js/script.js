4851728c1c55fa314bdd8e413ba495d561e9063d
Elai5 <yvettelaine007@gmail.com>
2024-01-24 09:16:52 +0300

    landingpage responsive, added js

---
+++ mysite/static/js/script.js
@@ -0,0 +1,22 @@
+function toggleNavbar() {
+    var navbar = document.getElementById("myNavbar");
+    if (navbar.className === "navbar") {
+        navbar.className += " responsive";
+    } else {
+        navbar.className = "navbar";
+    }
+}
+function previewProfileImage() {
+    var preview = document.getElementById('previewImage');
+    var fileINput = document.getElementById('profileImage');
+    var file = fileINput.files[0];
+
+    if (file) {
+        var reader = new FileReader();
+
+        reader.onload = function (e) {
+            preview.src = e.target.result;
+        };
+        reader.readAsDataURL(file)
+    }
+}
