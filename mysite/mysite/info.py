6a6a59952edcbcf83a8a509799559382a4cde0f6
Elai5 <yvettelaine007@gmail.com>
2025-05-28 15:52:38 +0300

    changes

--- mysite/mysite/info.py
+++ mysite/mysite/info.py
@@ -1,5 +1,7 @@
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'studentkonnect43@gmail.com'
-EMAIL_HOST_PASSWORD = 'ztjgxsoeuidfpung'
+# EMAIL_HOST_USER = 'studentkonnect43@gmail.com'
+# EMAIL_HOST_PASSWORD = 'ztjgxsoeuidfpung'
+EMAIL_HOST_USER = 'studentskonnect49@gmail.com'
+EMAIL_HOST_PASSWORD = 'sbpburjfslhtmqvl'
 EMAIL_PORT = 587
